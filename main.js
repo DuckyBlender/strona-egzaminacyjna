fccbb7a1eeab36d621f808460b41f4316a22a691
DuckyBlender <alan@alanklas.com>
2022-12-15 09:39:21 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log('Window size:', window.innerWidth, 'x', window.innerHeight);
+
+window.addEventListener('resize', () => {
+    console.log('Window size:', window.innerWidth, 'x', window.innerHeight);
+    // window.open("https://google.com", "Google", "width=1000, height=500", )
+});
